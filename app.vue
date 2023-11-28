4f2bf2b4771e4c247b2caed39423e5b4a11e7aa0
Dominik <djessen94@gmail.com>
2023-11-28 11:24:11 +0100

    Body styles and page meta

--- app.vue
+++ app.vue
@@ -22,10 +22,20 @@ if (locationQuery) {
 } else {
   forecast.value = null
 }
+
+useHead({
+  title: 'Nuxt Weather App',
+  meta: [
+    { name: 'description', content: 'Get the current weather forecast' }
+  ],
+  bodyAttrs: {
+    class: computed(() => 'min-h-screen bg-slate-50 text-slate-900'),
+  },
+});
 </script>
 
 <template>
-  <div class="flex flex-col gap-4 items-center">
+  <div class="flex flex-col gap-4 lg:gap-8 items-center justify-center w-11/12 md:w-4/5 mx-auto py-4">
 
     <div v-if="location">Weather <span>{{ location === 'Your City' ? 'at' : 'in' }}</span> {{ location }}</div>
     <button @click="$event => location = 'Your City'">Change location</button>
